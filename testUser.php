1b54e0637967c0fb832e08b6bce370dc24b9f7ee
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-02-09 22:35:14 +0100

    fix: Move Mailcatcher ports to default HTTP and SMTP ports to avoid extra PHP configuration

--- testUser.php
+++ testUser.php
@@ -6,9 +6,6 @@ use App\Model\User;
 use App\VO\Uid;
 use App\UserEntityManager;
 
-ini_set('SMTP', 'localhost');
-ini_set('smtp_port', '1025');
-
 $manager = new UserEntityManager();
 
 // Clear for testing
